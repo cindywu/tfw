0b80613acc2ef5a557a110c444b572c06b9759f7
Cindy Wu <ciindy.wu@gmail.com>
2024-11-01 12:15:50 -1000

    make it work

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,13 +8,34 @@ const openai = new OpenAI({
 
 export const runtime = "edge";
 
+interface Message {
+  role: "user" | "assistant" | "system";
+  content: string;
+}
+
 export async function POST(req: NextRequest) {
   const { messages } = await req.json();
+
+  console.log({ messages });
+
+  const modifiedMessages: Message[] = [
+    {
+      role: messages[messages.length - 1].role,
+      content:
+        "That feeling when " +
+        messages[messages.length - 1].content +
+        `. What three feelings is this person feeling? Answer this in three feeling words. Use only 3 words. The format of the feeling should fit in the sentence "I feel..." Do not include the "I feel..." part. For example, "I feel happy" would be "happy".`,
+    },
+  ];
+
+  console.log({ modifiedMessages });
+
   const response = await openai.chat.completions.create({
     model: "gpt-4",
     stream: true,
-    messages,
+    messages: modifiedMessages,
   });
+  console.log({ response });
   const stream = OpenAIStream(response);
   return new StreamingTextResponse(stream);
 }
